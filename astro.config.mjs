36823609a9134468718a24c656f51798fd04be7d
Davidciro-333 <davidcirortiz06@gmail.com>
2025-09-25 21:53:46 -0500

    Revert "astro.config.mjs changed"
    This reverts commit 089167b612f33f2d1ec3f247aa3e2e734bf4c088.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // Integración que añadiste
-import vercel from '@astrojs/vercel/serverless'; // Adaptador que añadiste
 
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
 export default defineConfig({
-  output: 'server', // Le dice a Astro que prepare el sitio para un servidor
-  adapter: vercel(), // Le dice a Astro cómo construir para Vercel
-  integrations: [tailwind()] // Lista de todas tus integraciones
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
